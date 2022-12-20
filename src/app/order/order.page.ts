401e52994d409619d99909e241b62a01e4b2e295
Ricardo Herrera <richard.andre.herrera@gmail.com>
2022-12-19 18:05:29 -0600

    navegacion

--- src/app/order/order.page.ts
+++ src/app/order/order.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-order',
   templateUrl: './order.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  toMenu(){
+    this.router.navigate(['/menu'])
+  }
 }
